b9f4c738d7705f87a7108e13bde661356f95a0ea
Pedro Piccichelli Carvalho <pedro.piccichelli@gmail.com>
2025-07-16 20:38:40 -0300

    Adicionar Model ConsultaItem

---
+++ DM113-AtendimentoMédico/Model/ConsultaItem.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+
+namespace DM113_AtendimentoMédico.Model
+{
+    [DataContract]
+    public class ConsultaItem
+    {
+        [DataMember]
+        public required DateTime Data { get; set; }
+        [DataMember]
+        public required string NomeCliente { get; set; }
+        [DataMember]
+        public required string CelularCliente { get; set; }
+
+    }
+}
